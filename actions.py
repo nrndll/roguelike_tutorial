71a5fc962d204da1c9f9971834ffd27ed7ab3db1
Nathan <nathan.rendall@gmail.com>
2021-08-18 14:37:42 +0100

    Part 1 complete

--- actions.py
+++ actions.py
@@ -1,15 +1,15 @@
 class Action:
     pass
 
-    # action when escape is pressed
-    class EscapeActions(Action):
-        pass
+# action when escape is pressed
+class EscapeAction(Action):
+    pass
 
 
-    # movement will take positional arguements, dx and dy, to handle player movement based on key inputs.
-    class MovementAction(Action):
-        def __init__(self, dx: int, dy: int):
-            super().__init__()
+# movement will take positional arguements, dx and dy, to handle player movement based on key inputs.
+class MovementAction(Action):
+    def __init__(self, dx: int, dy: int):
+        super().__init__()
 
-            self.dx = dx
-            self.dy = dy
+        self.dx = dx
+        self.dy = dy
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import tcod
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
@@ -12,6 +14,8 @@ def main() -> None:
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
         )
 
+    event_handler = EventHandler()
+
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -20,18 +24,25 @@ def main() -> None:
         vsync=True,
 
     ) as context:
-        
         root_console = tcod.Console(screen_width, screen_height, order="F")
         
         while True:
             root_console.print(x=player_x, y=player_y, string="@")
-
             context.present(root_console)
+            root_console.clear()
 
             for event in tcod.event.wait():
-                if event.type == "QUIT":
-                    raise SystemExit()
+                action = event_handler.dispatch(event)
+                
+                if action is None:
+                    continue
 
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
 
 if __name__ == "__main__":
     main()
